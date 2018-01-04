bee20771b2a3bfd34644986ed53f3be1075b4353
Ankit Kumar <ankit.kr.balyan@gmail.com>
2018-01-04 14:16:01 +0530

    :tada: Hip Hip Hurray!

---
+++ README.md
@@ -0,0 +1,28 @@
+# Express Trimmer
+
+An Express middleware to trim all incoming request values.
+
+## Installation
+```
+npm i express-trimmer
+```
+
+## How to use
+
+```
+const trimmer = require('trim-request');
+
+...
+// app initialization
+// use body parser
+...
+
+app.use(trimmer)
+```
+
+
+## License
+
+MIT © Ankit Balyan
+
+
---
+++ index.js
@@ -0,0 +1,35 @@
+function trimValues(input) {
+  if (input && input.constructor === Object) {
+    for (let prop in input) {
+      if (input[prop] && input[prop].constructor !== String) {
+        return trimValues(input[prop]);
+      }
+
+      if (input[prop] && input[prop].constructor === String && input.length) {
+        input[prop] = input[prop].trim();
+      }
+    }
+  } else if (input && input.constructor === Array && input.length) {
+    input.forEach(item => trimValues(item));
+  } else if (input && input.constructor === String && input.length) {
+    input = input.trim();
+  }
+}
+
+function expressTrimmer(req, res, next) {
+  if (req.body) {
+    trimValues(req.body);
+  }
+
+  if (req.params) {
+    trimValues(req.params);
+  }
+
+  if (req.query) {
+    trimValues(req.query);
+  }
+
+  next();
+}
+
+module.exports = expressTrimmer;
